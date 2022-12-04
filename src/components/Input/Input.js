d704fa4e37eaffc3933deef21a4e6dc90f356df6
Taimoor Fazal <taimoorf@uio.no>
2022-12-04 15:49:42 +0100

    refactor: add label option in input component

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -4,15 +4,18 @@ import "./Input.scss";
 
 function Input(props) {
   return (
-    <input
-      required={true}
-      className={props.InputClass}
-      type={props.Inputype}
-      name={props.InputName}
-      placeholder={props.InputPlaceholder}
-      value={props.value}
-      onChange={props.onChange}
-    />
+    <div className="input-container">
+      {props.label && <label>{props.label}</label>}
+      <input
+        required={true}
+        className={props.InputClass}
+        type={props.Inputype}
+        name={props.InputName}
+        placeholder={props.InputPlaceholder}
+        value={props.value}
+        onChange={props.onChange}
+      />
+    </div>
   );
 }
 
