8d98a396225f0b66b007ec485fa6f52e7a5321df
Taimoor Fazal <taimoorf@uio.no>
2022-12-04 01:20:00 +0100

    feat(loader-for-signup): set up loader for register user page

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -5,6 +5,8 @@ import Input from "../Input/Input";
 import ThemeBtn from "../ThemeBtn/ThemeBtn";
 import { register } from "../../api/LoginService";
 
+import LoadingOverlay from "react-loading-overlay";
+
 import "./Register.scss";
 
 //userTypeID = 1 because its for customers
@@ -24,6 +26,8 @@ function Register() {
   const [password, setPassword] = useState("");
   const [phone, setPhone] = useState("");
   const [response, setResponse] = useState("");
+  const [isLoading, setIsLoading]= useState(false);
+  
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -32,105 +36,127 @@ function Register() {
     user.email = email;
     user.password = password;
     user.phone = phone;
-
+    setIsLoading(true);
     register(user).then((response) => {
-      setResponse(response);
+      
+      if (response === "Success") {
+        clearForm();
+      }
+      setTimeout(()=>{setIsLoading(false); setResponse(response);}, 3000);
+
     });
   };
 
-  if (response === "Success") {
-    return <div>Success</div>;
-  } 
+  function clearForm() {
+    setFirstName("");
+    setLastName("");
+    setPassword("");
+    setPhone("");
+    setEmail("");
+  }
 
+  function timeFunction() {
+   
+}
   return (
-    <div className="Register_page">
-      <InnerHeader HeaderHeading="Register" PageText="Register" />
-      <section className="Register_main">
-        <div className="container">
-          <div className="row row_custom">
-            <div className="col-lg-6 col_form_otr">
-              <div className="col_form_inr">
-                <h3 className="heading-h3 form_heading">
-                  Register to Funtlers
-                </h3>
-                {response === "Failed" && (
-                  <div className="error_message">
-                    {
-                      "User with this email address already exists. Please try again with different email"
-                    }
-                  </div>
-                )}
-                <form onSubmit={handleSubmit} className="form_main">
-                  <div className="Input_otr">
-                    <Input
-                      InputClass="Theme_input_white form_input"
-                      Inputype="fname"
-                      InputName="fname"
-                      InputPlaceholder="First Name"
-                      value={firstName}
-                      onChange={(event) => setFirstName(event.target.value)}
-                    />
-                  </div>
-                  <div className="Input_otr">
-                    <Input
-                      InputClass="Theme_input_white form_input"
-                      Inputype="lname"
-                      InputName="lname"
-                      InputPlaceholder="Last Name"
-                      value={lastName}
-                      onChange={(event) => setLastName(event.target.value)}
-                    />
-                  </div>
-                  <div className="Input_otr">
-                    <Input
-                      InputClass="Theme_input_white form_input"
-                      Inputype="email"
-                      InputName="email"
-                      InputPlaceholder="Email Address"
-                      value={email}
-                      onChange={(event) => setEmail(event.target.value)}
-                    />
-                  </div>
-                  <div className="Input_otr">
-                    <Input
-                      InputClass="Theme_input_white form_input"
-                      Inputype="number"
-                      InputName="number"
-                      InputPlaceholder="Phone"
-                      value={phone}
-                      onChange={(event) => setPhone(event.target.value)}
-                    />
-                  </div>
-                  <div className="Input_otr">
-                    <Input
-                      InputClass="Theme_input_white form_input"
-                      Inputype="password"
-                      InputName="password"
-                      InputPlaceholder="Password"
-                      value={password}
-                      onChange={(event) => setPassword(event.target.value)}
-                    />
-                  </div>
-                  <div className="Input_otr action_otr">
-                    <button
-                      type="submit"
-                      className="Theme_btn_primary form_btn"
-                    >
-                      Register
-                    </button>
-                  </div>
-                  <div className="login_text_otr">
-                    <Link to="/login" className="login_text heading-sb">
-                      Login
-                    </Link>
-                  </div>
-                </form>
+    <LoadingOverlay active={isLoading} spinner text="Processing your request...">
+      <div className="Register_page">
+        <InnerHeader HeaderHeading="Register" PageText="Register" />
+        <section className="Register_main">
+          <div className="container">
+            <div className="row row_custom">
+              <div className="col-lg-6 col_form_otr">
+                <div className="col_form_inr">
+                  <h3 className="heading-h3 form_heading">
+                    Register to Funtlers
+                  </h3>
+                  {response === "Failed" && (
+                    <div className="error_message">
+                      {
+                        "User with this email address already exists. Please try again with different email."
+                      }
+                    </div>
+                  )}
+                  {response === "Success" && (
+                    <div className="success_message">
+                      {
+                        "User successfully registered! Please login to access your account!"
+                      }
+                    </div>
+                  )}
+
+                  <form onSubmit={handleSubmit} className="form_main">
+                    <div className="Input_otr">
+                      <Input
+                        InputClass="Theme_input_white form_input"
+                        Inputype="fname"
+                        InputName="fname"
+                        InputPlaceholder="First Name"
+                        value={firstName}
+                        onChange={(event) => setFirstName(event.target.value)}
+                      />
+                    </div>
+                    <div className="Input_otr">
+                      <Input
+                        InputClass="Theme_input_white form_input"
+                        Inputype="lname"
+                        InputName="lname"
+                        InputPlaceholder="Last Name"
+                        value={lastName}
+                        onChange={(event) => setLastName(event.target.value)}
+                      />
+                    </div>
+                    <div className="Input_otr">
+                      <Input
+                        InputClass="Theme_input_white form_input"
+                        Inputype="email"
+                        InputName="email"
+                        InputPlaceholder="Email Address"
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
+                      />
+                    </div>
+                    <div className="Input_otr">
+                      <Input
+                        InputClass="Theme_input_white form_input"
+                        Inputype="number"
+                        InputName="number"
+                        InputPlaceholder="Phone"
+                        value={phone}
+                        onChange={(event) => setPhone(event.target.value)}
+                      />
+                    </div>
+                    <div className="Input_otr">
+                      <Input
+                        InputClass="Theme_input_white form_input"
+                        Inputype="password"
+                        InputName="password"
+                        InputPlaceholder="Password"
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
+                      />
+                    </div>
+                    <div className="Input_otr action_otr">
+                      <button
+                        type="submit"
+                        className="Theme_btn_primary form_btn"
+                      >
+                        Register
+                      </button>
+                    </div>
+                    <div className="login_text_otr">
+                      <Link to="/login" className="login_text heading-sb">
+                        Login
+                      </Link>
+                    </div>
+                  </form>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
-    </div>
+        </section>
+      </div>
+    </LoadingOverlay>
   );
 }
 
