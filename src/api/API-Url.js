7536405481cae84f92af40cf4688ee5adfc10aff
Taimoor Fazal <taimoorf@uio.no>
2022-12-12 00:12:41 +0100

    feat: change backend URL

--- src/api/API-Url.js
+++ src/api/API-Url.js
@@ -1,5 +1,9 @@
 export default function apiURL()
 {
-    const url="https://localhost:44360/";
+    //live url
+    const url="http://tecknerds-001-site1.ctempurl.com/";
+    // local dev url
+   // const url="https://localhost:44360/";
+    
     return url;
 }
