62cd445b8d4cf5eba5271489264ba44d9081b3c0
Taimoor Fazal <taimoorf@uio.no>
2022-12-14 02:54:26 +0100

    fix: add option to use search result card to enable different routes based on role

--- src/components/SearchResultCard/SearchResultCard.js
+++ src/components/SearchResultCard/SearchResultCard.js
@@ -1,36 +1,41 @@
-import React from 'react'
-import './SearchResultCard.scss';
-import { Link, useNavigate } from 'react-router-dom';
-
+import React from "react";
+import "./SearchResultCard.scss";
+import { Link, useNavigate } from "react-router-dom";
 
 function SearchResultCard(props) {
+  let activity;
 
-    let activity = props.Data;
+  //the rerouting from this component will be different if it is coming from customer side.
+  if (props.source === "customer") activity = props.Data;
+  else activity = props.activity;
 
-    const navigate = useNavigate();
-    const toActivityDetails = () => {
-        navigate('/activity-detail', { state: activity });
-    }
-    return (
-        <div className='col-lg-3 col_searchCard_otr' onClick={() => { toActivityDetails() }}>
-            <div className='col_searchCard_inr'>
-                <div className='img_otr'>
-                    <div className={`${props.IconDivClass} icon_otr`}>
-                        <i class={`${props.IconClass} edit_icon`}></i>
-                    </div>
-                    <img className='card_img' src={props.CardImg} alt='img' />
-                </div>
-                <div className='content'>
-                    <Link className='heading-lb card_heading'>
-                        {props.CardHeading}
-                    </Link>
-                    <p className='heading-s card_desc'>
-                        {props.CardDesc}
-                    </p>
-                </div>
-            </div>
+  const navigate = useNavigate();
+  const toActivityDetails = () => {
+    if (props.source === "customer")
+      navigate("/activity-detail", { state: activity });
+    else navigate("/edit-activity", { state: activity });
+  };
+  return (
+    <div
+      className="col-lg-3 col_searchCard_otr"
+      onClick={() => {
+        toActivityDetails();
+      }}
+    >
+      <div className="col_searchCard_inr">
+        <div className="img_otr">
+          <div className={`${props.IconDivClass} icon_otr`}>
+            <i class={`${props.IconClass} edit_icon`}></i>
+          </div>
+          <img className="card_img" src={props.CardImg} alt="img" />
         </div>
-    )
+        <div className="content">
+          <Link className="heading-lb card_heading">{props.CardHeading}</Link>
+          <p className="heading-s card_desc">{props.CardDesc}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default SearchResultCard
+export default SearchResultCard;
