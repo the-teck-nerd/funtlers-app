7897a4020017929e6f5ed21067aeeef56c2c1792
Taimoor Fazal <taimoorf@uio.no>
2022-12-21 02:40:10 +0100

    search activity changes

--- src/components/SearchResultPage/SearchResultPage.js
+++ src/components/SearchResultPage/SearchResultPage.js
@@ -29,10 +29,10 @@ function SearchResultPage() {
   const [city, setCity] = useState(searchFilter?.city);
   const [type, setType] = useState(searchFilter?.type);
 
+  let categoryLabel =
+    getLabelByValue(filters.categories, searchFilter?.category) ?? "";
 
-  let category = getLabelByValue(filters.categories, searchFilter?.category);
-  
-
+  const category = searchFilter?.category ?? "";
   const [peopleNumber, setPeopleNumber] = useState(searchFilter?.peopleNumber);
   const [budget, setBudget] = useState(searchFilter?.budget);
 
@@ -43,6 +43,7 @@ function SearchResultPage() {
   const [isLoading, setIsLoading] = useState(false);
 
   const fetchData = () => {
+    setIsLoading(true);
     let ownerid = 1;
     let apicall = FetchService.getAllActivities(ownerid);
 
@@ -61,11 +62,13 @@ function SearchResultPage() {
         filterModal.budget = budget === "" ? "all" : budget;
 
         setFilteredActivities(getFilteredActivities(data, filterModal));
+        setTimeout(() => {
+          setIsLoading(false);
+        }, 1000);
       });
   };
 
   useEffect(() => {
-    setIsLoading(true);
     fetchData();
   }, []);
 
@@ -101,12 +104,12 @@ function SearchResultPage() {
           PageText="Søkeresultat fysisk"
         />
         {/* //todo: show the label not the actual value */}
-        <h2 className="padding header_heading text-center">{category}</h2>
+        <h2 className="padding header_heading text-center">{categoryLabel}</h2>
 
         <div className="searchRsult_main">
           <div className="container">
             <div className="wrapper">
-              <h3 className="heading-h3 filter_heading">
+              <h3 className="heading filter_heading">
                 Aktivitet (
                 {filteredActivities.length > 0 ? filteredActivities.length : 0})
               </h3>
@@ -127,6 +130,7 @@ function SearchResultPage() {
                         setValue={setCity}
                         options={filters.cities}
                         defaultText="Cities"
+                        disabled={type === "digital"}
                       />
                     </li>
                     <li className="activity_li">
@@ -146,12 +150,11 @@ function SearchResultPage() {
                       />
                     </li>
                     <li className="activity_li">
-                    <button
-                            class="ri-search-2-line search_icon_local search-button"
-                            type="submit"
-                          ></button>
-                      </li>
-
+                      <button
+                        class="ri-search-2-line search_icon_local search-button"
+                        type="submit"
+                      ></button>
+                    </li>
                   </ul>
                 </div>
                 {/* todo: hide it for now */}
