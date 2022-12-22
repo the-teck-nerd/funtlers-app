d8847cef5dd26db275e46ddfb3133ed179b2e5dd
Taimoor Fazal <taimoorf@uio.no>
2022-12-22 01:01:46 +0100

    change live url for backend

--- src/api/API-Url.js
+++ src/api/API-Url.js
@@ -1,9 +1,9 @@
 export default function apiURL()
 {
     //live url
-//const url="http://tecknerds-001-site1.ctempurl.com/";
+   const url="http://tecknerds-001-site1.ctempurl.com/";
     // local dev url
-   const url="https://localhost:44360/";
+   //const url="https://localhost:44360/";
     
     return url;
 }
